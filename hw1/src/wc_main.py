b05bfb7253b13930c9f3dd6d88f6fc584ef4905e
Andrey <56671855+Syslikss@users.noreply.github.com>
2024-03-04 00:24:30 +0300

    wc refactoring

--- hw1/src/wc_main.py
+++ hw1/src/wc_main.py
@@ -2,6 +2,8 @@ import argparse
 import re
 import sys
 
+WORD_PATTERN = re.compile(r'\w+')
+
 
 def get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description='Python version of the wc command')
@@ -14,16 +16,21 @@ def get_parser() -> argparse.ArgumentParser:
     return parser
 
 
-word_pattern = re.compile(r'\w+')
-
-
 def count_lines_words_bytes(file) -> (int, int, int):
     lines_count, words_count, bytes_count = 0, 0, 0
-    for line in file:
-        lines_count += 1
-        words_count += len(word_pattern.findall(line))
-        bytes_count += len(line.encode('utf-8'))
+    try:
+        for line in file:
+            lines_count += 1
+            words_count += len(WORD_PATTERN.findall(line))
+            bytes_count += len(line.encode('utf-8'))
+    except (KeyboardInterrupt, EOFError):
+        pass
     return lines_count, words_count, bytes_count
+
+
+def print_wc_summary(total_lines_count, total_words_count, total_bytes_count, additional_info=None):
+    print(
+        f'{total_lines_count:>6} {total_words_count:>6} {total_bytes_count:>6} {additional_info if additional_info else ""}')
 
 
 def main():
@@ -31,21 +38,25 @@ def main():
     args = parser.parse_args()
     total_lines_count, total_words_count, total_bytes_count = 0, 0, 0
     if args.files[0] == '-':
-        try:
-            for line in sys.stdin:
-                total_lines_count += 1
-                total_words_count += len(word_pattern.findall(line))
-                total_bytes_count += len(line.encode('utf-8'))
-        except (KeyboardInterrupt, EOFError):
-            pass
+        total_lines_count, total_words_count, total_bytes_count = count_lines_words_bytes(sys.stdin)
+        print_wc_summary(total_lines_count,
+                         total_words_count,
+                         total_bytes_count)
     else:
         for file in args.files:
             lines_count, words_count, bytes_count = count_lines_words_bytes(file)
             total_bytes_count += bytes_count
             total_words_count += words_count
             total_lines_count += lines_count
-            print(f'{lines_count} {words_count} {bytes_count} {file.name}')
-    print(f'{total_lines_count} {total_words_count} {total_bytes_count} {"total" if len(args.files) > 1 else ""}')
+            print_wc_summary(lines_count,
+                             words_count,
+                             bytes_count,
+                             file.name)
+        if len(args.files) > 1:
+            print_wc_summary(total_lines_count,
+                             total_words_count,
+                             total_bytes_count,
+                             "total" if len(args.files) > 1 else "")
 
 
 if __name__ == '__main__':
